d0428866620e40034c7b245eff7177d638b76ec6
TheHarald <r.surinov@netris.ru>
2025-08-21 14:04:46 +0300

    add weight request button

--- src/modules/statistics/index.tsx
+++ src/modules/statistics/index.tsx
@@ -2,6 +2,7 @@ import { observer } from "mobx-react-lite";
 import { useEffect } from "react";
 import { statisticsStore } from "./services/statistics-store";
 import { StatisticsWeightGraphic } from "./components/statistics-weight-graphic";
+import { Button } from "@heroui/react";
 
 export const StatisticsModule = observer(() => {
   useEffect(() => {
@@ -11,8 +12,11 @@ export const StatisticsModule = observer(() => {
   const { weights } = statisticsStore;
 
   return (
-    <>
+    <div className="flex flex-col gap-4">
       <StatisticsWeightGraphic weights={weights} />
-    </>
+      <Button onPress={() => statisticsStore.setOpen(true)} color="primary">
+        Заполнить показания веса
+      </Button>
+    </div>
   );
 });
