c2180eedb7e1dc0f633e05f160dbcc2b3d2f3a90
TheHarald <r.surinov@netris.ru>
2025-08-23 15:22:35 +0300

    add confirmation

--- src/modules/settings/index.tsx
+++ src/modules/settings/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@heroui/react";
 import { BuildInfo } from "./components/build-info";
 import { TrainingSettingsEditor } from "./components/training-settings-editor";
+import { ConfirmationWrapper } from "../../components/confirmation-wrapper";
 
 export const SettingsModule = () => {
   return (
@@ -8,9 +9,17 @@ export const SettingsModule = () => {
       <h1 className="text-2xl font-bold">Настройки</h1>
       <BuildInfo />
       <TrainingSettingsEditor />
-      <Button color="danger" onPress={() => localStorage.clear()}>
-        Очистить хранилище
-      </Button>
+
+      <ConfirmationWrapper
+        color="danger"
+        onConfirm={() => localStorage.clear()}
+        title="Очистка данных"
+        message="Вы уверены, что хотите очистить все данные? Это действие нельзя отменить."
+        confirmText="Очистить"
+        cancelText="Отменить"
+      >
+        <Button color="danger">Очистить хранилище</Button>
+      </ConfirmationWrapper>
     </div>
   );
 };
