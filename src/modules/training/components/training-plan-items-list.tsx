b0918f21d34e49ca7dec663cbcf2bb3fcf560bc4
TheHarald <r.surinov@netris.ru>
2025-08-24 15:43:38 +0300

    add training button

--- src/modules/training/components/training-plan-items-list.tsx
+++ src/modules/training/components/training-plan-items-list.tsx
@@ -4,13 +4,24 @@ import { TrainingPlanItem } from "./training-plan-item";
 import { Button, ScrollShadow } from "@heroui/react";
 import { navigationStore } from "../../navigation/services/navigation-store";
 import { AppRoutes } from "../../navigation/services/types";
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
 
 export const TrainingPlanItemsList = observer(() => {
   const { trainings } = trainingStore;
 
   return (
     <div className="flex flex-col gap-4 flex-1 overflow-hidden">
-      <h2 className="text-2xl font-bold">Тренировки</h2>
+      <div className=" flex flex-row items-center justify-between">
+        <h2 className="text-2xl font-bold">Тренировки</h2>
+        <Button
+          isIconOnly
+          color="primary"
+          variant="light"
+          onPress={() => navigationStore.setTab(AppRoutes.Constructor)}
+        >
+          <PlusCircleIcon className="size-6" />
+        </Button>
+      </div>
       {(() => {
         if (trainings.length === 0) {
           return (
